33f81eb2f3828a1e491c2005257ce88d7ee6c36a
FYBOO7 <fbijapure.fb@gmail.com>
2024-02-14 22:34:32 +0530

    TestCase 4

---
+++ src/TestCase4/Logout.java
@@ -0,0 +1,52 @@
+package TestCase4;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Logout {
+	WebDriver driver;
+	@BeforeTest
+	public void initilize() 
+	{
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://automationexercise.com/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	@Test
+	public void Valid() {
+		
+		String title =driver.getTitle();
+		String expectedTitle= "Automation Exercise";
+		Assert.assertEquals(title, expectedTitle, "Page title doesn't match the expected title");
+		
+		driver.findElement(By.xpath("//li//a[text()=\" Signup / Login\"]")).click();
+		String headingLogin= driver.findElement(By.xpath("//div[@class=\"login-form\"]//h2")).getText();
+		String expectedLoginTitle= "Login to your account";
+		Assert.assertEquals(headingLogin, expectedLoginTitle, "Page title doesn't match the expected title");
+		
+		driver.findElement(By.xpath("//form//input[@data-qa=\"login-email\"]")).sendKeys("fbijapure.fb@gmail.com");
+		driver.findElement(By.xpath("//form//input[@data-qa=\"login-password\"]")).sendKeys("Bijapurefb");
+		driver.findElement(By.xpath("//form//button[@data-qa=\"login-button\"]")).click();
+		
+		String user =driver.findElement(By.xpath("//li//a[text()=\" Logged in as \"]")).getText();
+		String expectedUserTitle= "Logged in as Farhan Biajpure";
+		Assert.assertEquals(user, expectedUserTitle, "Page title doesn't match the expected title");
+		
+		driver.findElement(By.xpath("//li//a[@href=\"/logout\"]")).click();
+		Assert.assertEquals(headingLogin, expectedLoginTitle, "Page title doesn't match the expected title");
+	}
+	@AfterTest
+	public void close() {
+		driver.close();
+	}
+}
